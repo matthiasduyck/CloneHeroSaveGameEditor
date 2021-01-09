e31636832a56e146b55b0c61c72871277326c38c
Matthias <matthiasduyck@gmail.com>
2021-01-09 14:02:37 +0100

    Project setup, start modeling files

---
+++ CloneHeroSaveGameEditor/CloneHeroSaveGameEditor/Models/ScoreEntry.cs
@@ -0,0 +1,29 @@
+﻿namespace CloneHeroSaveGameEditor.Models
+{
+    class ScoreEntry
+    {
+        string SongIdentifier;// first part of the score entry, 20 character long hex string, can be linked in the songcache.bin file to the actual file // blok(h)9F-BE length (h)20
+        //todo some unknown field at 29 after identifier
+        int PlayCount;//simple hex number at 2A
+        //todo 4 hex fields I don't understand from 2B to 2E, maybe just padding???
+        int Difficulty;//hex number at 2F representing difficulty, 02=easy 03=medium
+        int Percentage;//hex number at 30 representing percentage score
+        bool HasCrown;//hex value at 31, looks like a bool indicating the 'crown'???
+        //todo 4 hex fields I don't understand yet from 32-35, probably contains some useful data, maybe modifiers?
+        bool Score; //hex number from 36 to 39, 4 long, contains score in little endian int32
+
+
+        public byte[] ConvertToBytesArray()
+        {
+            
+            return null;//todo
+        }
+
+        public ScoreEntry(byte[] fromBytes)
+        {
+            //todo decode frombytes to all relevant data
+        }
+    }
+
+    
+}
---
+++ CloneHeroSaveGameEditor/CloneHeroSaveGameEditor/Models/ScoresData.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CloneHeroSaveGameEditor.Models
+{
+    class ScoresData
+    {
+        private string weirdheadertodo;//not sure what this should be or represents
+        List<ScoreEntry> ScoreEntries;
+    }
+}
